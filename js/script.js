e56aea422a7768e0781363ddd19b3e34fd930e67
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-12 13:33:38 +0800

    updt-selesai

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const tanah = document.querySelectorAll('.tanah');
 const tikus = document.querySelectorAll('.tikus');
+const papanSkor = document.querySelector('.papan-skor');
 
 let tanahSebelumnya;
+let selesai;
+let skor;
+
 
 function randomTanah(tanah){
     const t = Math.floor(Math.random() * tanah.length);
@@ -15,11 +19,38 @@ function randomTanah(tanah){
     return tRandom;
 }
 
+function randomWaktu(min, max){
+    return Math.round(Math.random() * (max-min) + min);
+}
+
 function munculkanTikus(){
     const tRandom = randomTanah(tanah)
+    const wRandom = randomWaktu(200, 1500);
     tRandom.classList.add('muncul');
 
     setTimeout(() => {
         tRandom.classList.remove('muncul');
-    }, 500);
+        if(!selesai) 
+            {munculkanTikus();}
+    }, wRandom);
 }
+
+function mulai(){
+    selesai=false;
+    skor=0;
+    papanSkor.textContent = 0;
+    munculkanTikus();
+    setTimeout(() => {
+        selesai = true;
+    }, 10000);
+}
+
+function pukul(){
+    skor++;
+    papanSkor.textContent=skor;
+    this.parrentNode.classList.remove('muncul');
+}
+
+tikus.forEach(t => {
+    t.addEventListener('click', pukul);
+})
